e33415cf9521f067ccc38fd696c52ba65af9dc6d
Maria Terekhina <maria.terekhina@highspot.com>
2020-10-28 21:02:51 -0700

    sass global setup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Nav from "../components/Nav";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Nav from '../components/Nav';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>The Evolve Experience</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
