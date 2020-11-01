02c7f1a7059380e5d7195bb55a281e7d716b24aa
Maria Terekhina <maria.terekhina@highspot.com>
2020-11-01 11:02:31 -0800

    removing bug

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Nav from "../components/Nav";
-import Link from "../components/Link/Link";
-import ImageLink from "../components/Link/ImageLink";
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Nav from '../components/Nav';
+import Link from '../components/Link/Link';
+import ImageLink from '../components/Link/ImageLink';
 
 export default function Home() {
   return (
-    <Html lang="en">
-      <div className={styles.container}>
-        <Head>
-          <title>The Evolve Experience</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Nav />
-        <main className={styles.main}>
-          <h1>Welcome to the home page!</h1>
-        </main>
+    <div className={styles.container}>
+      <Head>
+        <title>The Evolve Experience</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Nav />
+      <main className={styles.main}>
+        <h1>Welcome to the home page!</h1>
+      </main>
 
-        <footer className={styles.footer}></footer>
-      </div>
-    </Html>
+      <footer className={styles.footer}></footer>
+    </div>
   );
 }
