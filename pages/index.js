fb6a7db8cd3d654c69e097c8a1e749c3e60d6e25
Maria Terekhina <maria.terekhina@highspot.com>
2020-10-31 22:19:12 -0700

    remove meta

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
         <Head>
           <title>The Evolve Experience</title>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <Nav />
         <main className={styles.main}>
