ade93769815bfadc5935a25b2b099a2930fd15e7
kat-perreira <kat.perreira@highspot.com>
2020-11-04 12:10:41 -0800

    addressed PR comments

--- pages/monologues.js
+++ pages/monologues.js
@@ -49,13 +49,8 @@ const Monologues = () => {
               </p>
               <section className={styles.sectionVideo}>
                 <div className={clsx(['section-content', styles.video])}>
-                  <div class={styles.embedContainer}>
-                    <iframe
-                      width="560"
-                      height="315"
-                      src="https://www.youtube.com/embed/aoT1Krp9HF4"
-                      frameborder="0"
-                    ></iframe>
+                  <div className={styles.embedContainer}>
+                    <iframe src="https://www.youtube.com/embed/aoT1Krp9HF4" frameborder="0"></iframe>
                   </div>{' '}
                 </div>
               </section>
@@ -103,10 +98,8 @@ const Monologues = () => {
               </p>
               <section className={styles.sectionVideo}>
                 <div className={clsx(['section-content', styles.video])}>
-                  <div class={styles.embedContainer}>
+                  <div className={styles.embedContainer}>
                     <iframe
-                      width="560"
-                      height="315"
                       src="https://www.youtube.com/embed/6YUH2Oj_0zE"
                       frameborder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
