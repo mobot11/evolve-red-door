fa5a9ee0076236d3f3cd6c4ca9f1362eeaacbc6a
Maria Terekhina <maria.terekhina@highspot.com>
2020-11-04 18:49:52 -0800

    fixing empty links

--- components/Link/IconLink.js
+++ components/Link/IconLink.js
@@ -7,26 +7,32 @@ import clsx from 'clsx';
 const IconLink = ({ variant, text, anchorProps = {}, theme }) => {
   let icon;
   let href;
+  let label;
   switch (variant) {
     case 'facebook':
       icon = <FaceBookIcon />;
       href = 'https://www.facebook.com/reddoorprojectpdx';
+      label = `access The Red Door Project's Facebook page`;
       break;
     case 'twitter':
       href = 'https://twitter.com/reddoorproject?s=20';
       icon = <TwitterIcon />;
+      label = `access The Red Door Project's Twitter page`;
       break;
     case 'email':
       href = 'mailto:info@reddoorproject.org';
       icon = <EmailIcon />;
+      label = `email The Red Door Project`;
       break;
     case 'instagram':
       href = 'https://www.instagram.com/reddoorproject/';
       icon = <InstagramIcon />;
+      label = `access The Red Door Project's Instagram page`;
       break;
     case 'phone':
       href = 'tel:1-503-541-0739';
       icon = <PhoneIcon />;
+      label = `contact telephone number 1 503 541 07 39`;
       break;
     default:
       throw new Error('invalid variant supplied to IconLink');
@@ -35,6 +41,7 @@ const IconLink = ({ variant, text, anchorProps = {}, theme }) => {
     <div>
       <a href={href} className={clsx(styles[`icon-${theme}`], styles.iconLink)} {...anchorProps}>
         {icon} {text && <span className={clsx(styles.iconLinkText, 'icon-text')}>{text}</span>}
+        <span class="visuallyhidden">{label}</span>
       </a>
     </div>
   );
