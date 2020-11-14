f9f1ea0cfdab5b8f66b2662f9e3e8a1cd7134ebb
Sammy Hendrick <qbnflaco@gmail.com>
2020-11-13 17:53:24 -0800

    Adding id to monologue main el for skip links.

--- pages/monologues.js
+++ pages/monologues.js
@@ -18,7 +18,7 @@ const Monologues = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <PageHeader />
-      <main className="main">
+      <main className="main" id="main">
         <div className={clsx(['section', styles.sectionBlack])}>
           <div className={clsx(['section-content', 'hero', styles.img])}></div>
         </div>
