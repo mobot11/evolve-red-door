90888358dcce93ab5f2c662e98abefb741b1eb2f
Maria Terekhina <maria.terekhina@highspot.com>
2020-11-19 15:17:04 -0800

    safari error

--- components/Video.js
+++ components/Video.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Video = ({ title, src }) => {
-  return <iframe title={title} src={`${src}?cc_load_policy=1&cc_lang_pref=en`}></iframe>;
+  return <iframe allowFullScreen title={title} src={`${src}?cc_load_policy=1&cc_lang_pref=en`}></iframe>;
 };
 
 Video.propTypes = {
